2c71a2d9ddc3a32984b4b7b94a675f8ebe269281
HGG <52917408+hgg-dev@users.noreply.github.com>
2020-02-29 11:02:20 +1100

    test

---
+++ build/precache-manifest.ce67238c5cfb07ea24cdeced9db662ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3022a9afdfaf285e7a7bc7e846214a63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b19ee8416d91bc5ef6c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fb458fbc070416653d57",
+    "url": "/static/js/2.1da5956d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1da5956d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b19ee8416d91bc5ef6c",
+    "url": "/static/js/main.61c32dc0.chunk.js"
+  },
+  {
+    "revision": "adbb2c4021b50985ddc5",
+    "url": "/static/js/runtime-main.dad863ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
